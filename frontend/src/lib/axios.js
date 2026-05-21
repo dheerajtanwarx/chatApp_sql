785cf4db3da5fc7c9dfbf72d9511ef022fa6d14d
dheerajsaini01 <tanwardheeraj00111@gmail.com>
2026-05-21 17:06:15 +0530

    fix: update base URL configuration for production environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //Frontend → Backend se baat karne ke liye axios use hota hai
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:4000/api" : import.meta.env.VITE_API_URL || "https://chatapp-sql.onrender.com/api",
     withCredentials: true,
       headers: {
     "Content-Type": "application/json"
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast'
 import { create } from "zustand";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : "/"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000" : import.meta.env.VITE_API_URL || "https://chatapp-sql.onrender.com"
 
 export const useAuthStore = create((set, get) => ({
     //currently logged in user
