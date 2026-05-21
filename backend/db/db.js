1193c655c4625c16259a90a5a270ad36083bde7e
dheerajsaini01 <tanwardheeraj00111@gmail.com>
2026-05-21 17:45:49 +0530

    Connected Aiven MySQL

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,12 +2,28 @@ import mysql from 'mysql2/promise'
 import dotenv from "dotenv";
 dotenv.config()
 
-const db = mysql.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
 
-})
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const connectDB = async () => {
+  try {
+    const connection = await db.getConnection();
+    console.log("Connected to Aiven MySQL");
+    connection.release();
+  } catch (err) {
+    console.log("DB connection failed:", err);
+  }
+};
+
+connectDB();
 
 export {db}
